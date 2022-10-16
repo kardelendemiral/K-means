8fa8ecdcfd79cc5f1efddf52cc4ea70db488ae99
kardelendemiral <74200767+kardelendemiral@users.noreply.github.com>
2022-10-16 19:38:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from sklearn.cluster import KMeans
 colours = ["b", "g", "r", "c", "m", "y", "k", "w"]
 colors = ['blue', 'red', 'green', 'yellow', 'purple', 'darkblue', 'pink', 'orange']
 
-MAX_ITERATIONS = 10
+MAX_ITERATIONS = 15
 Js = []
 
 def calculateWCSS(df, k):
@@ -20,12 +20,14 @@ def calculateWCSS(df, k):
 def elbowMethod(df):
     plt.clf()
     w = []
-    for k in range(1,6):
+    for k in range(1,9):
         w.append(calculateWCSS(df,k))
 
-    plt.plot(range(1,6), w)
+    plt.plot(range(1,9), w)
     plt.show()
     plt.clf()
+
+
 
 def getJ(df, centroidsX, centroidsY):
 
@@ -58,22 +60,49 @@ def getRandomCentroids(df, k):
 
     return selectedX, selectedY
 
+def generateDifficultData(k, size):
+    weights = []
+    heights = []
+    startingPosY = []
+
+    for i in range(k):
+        weights.append(random.randrange(5,20))
+        heights.append(random.randrange(10,50))
+        startingPosY.append(random.randrange(0,30))
+
+    startingPosX = []
+
+    for i in range(k):
+        if i == 0:
+            startingPosX.append(1)
+        else:
+            startingPosX.append(weights[i - 1] + 5)
+
+    x = []
+    y = []
+
+    for i in range(k):
+        for j in range(heights[i]*10):
+            y.append(startingPosY[i] + j)
+            x.append(startingPosX[i] + random.randrange(0, weights[i]) + random.random())
+
+    c = [None]*len(x)
+    df = pd.DataFrame({'c': c,'x': x, 'y': y})
+    return df
+
 def generateData(k, size):
     x = []
     y = []
 
     for cluster in range(k):
-        meanX = np.random.uniform(0, 70)
-        meanY = np.random.uniform(0, 70)
+        meanX = np.random.uniform(0, k*15)
+        meanY = np.random.uniform(0, k*15)
         sigma = np.random.uniform(3, 8)
         x.extend(np.random.normal(meanX, sigma, size))
         y.extend(np.random.normal(meanY, sigma, size))
 
     c = [None]*size*k
     df = pd.DataFrame({'c': c, 'x': x, 'y': y})
-    #plt.scatter(x, y)
-    #plt.show()
-    #plt.clf()
     return df
 
 def kMeans(df, k):
@@ -112,12 +141,11 @@ def kMeans(df, k):
         plt.plot(centroidsX[i], centroidsY[i], "k.", markersize=10)
 
    # plt.plot(x, y, '.', label=list(df['c']), alpha=0.5)
-   # plt.legend()
+    plt.title("Iteration 1")
     plt.show()
 
     Js.append(getJ(df, centroidsX, centroidsY))
     new_centroids = pd.DataFrame(df).groupby(by='c').mean().values
-    #print(len(new_centroids))
     centroidsX = []
     centroidsY = []
 
@@ -155,7 +183,6 @@ def kMeans(df, k):
             plt.plot(centroidsX[i], centroidsY[i], "k.", markersize=10)
 
         # plt.plot(x, y, '.', label=list(df['c']), alpha=0.5)
-        # plt.legend()
         plt.show()
 
         Js.append(getJ(df, centroidsX, centroidsY))
@@ -170,63 +197,34 @@ def kMeans(df, k):
     return centroidsX, centroidsY
 
 
-k = 4
+k = 3
 
 df = generateData(k, 1000)
-kMeans(df, k)
+
+#df = generateDifficultData(k, 1000)
+
+plt.scatter(df['x'], df['y'], s=0.5)
+plt.title("Dataset")
+plt.show()
+plt.clf()
+
+centroidsX, centroidsY = kMeans(df, k)
+centers = []
+
+for i in range(k):
+    centers.append([centroidsX[i], centroidsY[i]])
 
 plt.clf()
 plt.plot(range(MAX_ITERATIONS+1), Js)
 plt.show()
 
+kmeans = KMeans(n_clusters=k, init="k-means++")
+kmeans = kmeans.fit(df[['x', 'y']])
+
+print("Found centroids using my method:")
+print(centers)
+print("Found centroids using sklearn library:")
+print(kmeans.cluster_centers_)
+
 #elbowMethod(df)
 
-
-
-#plt.scatter(x, y, alpha = 0.6, s=10)
-#plt.show()
-
-
-
-
-"""d_means = {'cluster 1': [0, 0],
-           'cluster 2': [4, 5],
-           'cluster 3': [5, 0]}
-d_covs = {'cluster 1': [[1, 1],
-                        [1, 4]],
-          'cluster 2': [[1, 1],
-                        [1, 3]],
-          'cluster 3': [[4, 2],
-                        [2, 2]]}
-
-
-
-
-
-# Generate data based on the above parameters
-n_data = 1000
-
-# Generate data based on the above parameters
-data = []
-n_data = 1000
-"""
-
-
-
-"""
-for cluster in d_means.keys():
-    arr = np.random.multivariate_normal(d_means[cluster], d_covs[cluster], n_data)
-    df_tmp = pd.DataFrame(arr)
-    df_tmp['label'] = cluster
-    l.append(df_tmp)
-    plt.plot(df_tmp[0], df_tmp[1], '.', label=cluster, alpha=0.5)
-
-plt.legend()
-plt.axis('off')
-plt.show()"""
-
-"""features, clusters = make_blobs(n_samples=2000, n_features=2, centers=3, cluster_std=0.4, shuffle=True)
-
-plt.scatter(features[:,0], features[:,1])
-
-plt.show()"""
